a5038f698b0392d59a89076da2c280b41e7baa17
Bernard O'Connor <bernardoconnor1001@gmail.com>
2021-04-17 00:52:36 +0100

    Add support for building right side walls

--- BuildWallFloor.lua
+++ BuildWallFloor.lua
@@ -10,7 +10,7 @@ function buildWall(isLeftWall, height, width)
             print("dig")
             util.digAndPlace(2, util.directions.forward)
             if isOnBottomSide then
-                if curHeight < height then
+                if curHeight < height then -- todo move check up a level
                     util.move(1, util.directions.up)
                 end
             elseif curHeight < height then
@@ -24,9 +24,16 @@ function buildWall(isLeftWall, height, width)
 
          -- check if should move into next width row
         if curWidth < width then
-            turtle.turnRight()
-            util.move()
-            turtle.turnLeft()
+            if isLeftWall then
+                turtle.turnRight()
+                util.move()
+                turtle.turnLeft()
+            else
+                turtle.turnLeft()
+                util.move()
+                turtle.turnRight()
+            end
+            
         end
     end
 
@@ -34,30 +41,43 @@ function buildWall(isLeftWall, height, width)
     if not isOnBottomSide then
         util.move(height - 1, util.directions.down)
     end
-    turtle.turnLeft()
-    util.move(width - 1, util.directions.forward)
-    turtle.turnRight()
+    if isLeftWall then
+        turtle.turnLeft()
+        util.move(width - 1, util.directions.forward)
+        turtle.turnRight()
+    else
+        turtle.turnRight()
+        util.move(width - 1, util.directions.forward)
+        turtle.turnLeft()
+    end
 
 end
 
 
-function validateInputs(width, height)
+function validateInputs(leftWall, width, height)
     if  width < 1 or height < 1 then
         error("All dimensions should be larger than 0")
     end
+
+    if not(leftWall == 'y' or leftWall == 'n') then
+        error("Enter \"y\" or \"n\" for what side the wall is on")
+    end
 end
 
-print("Assume turtle is placed at bottom left facing / on top of the first block")
+print("Assume turtle is placed at bottom left/right,  facing / on top of the first block")
+
+print("Is the wall on the left? (y/n)")
+local leftWall = read()
 
 print("Enter width: ")
 local width = tonumber(read())
 print("Enter height: ")
 local height = tonumber(read())
 
-validateInputs(width, height)
+validateInputs(leftWall, width, height)
 
 util.topUpFuel(true)
 print("Time to build")
-buildWall(true, height, width)
+buildWall(leftWall == "y", height, width)
 util.printFuelLevel()
 print("Complete")
